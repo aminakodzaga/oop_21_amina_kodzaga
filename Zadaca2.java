5018438336137ba261a87150e5c30f231d84a2f9
aminakodzaga <92090852+aminakodzaga@users.noreply.github.com>
2021-10-19 17:24:02 +0200

    Add files via upload

---
+++ Zadaca2.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package zadaca2;
+
+/**
+ *
+ * @author PC
+ */
+public class Zadaca2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* Scanner reader=new Scanner(System.in);
+        String correct_pass = "carrot";
+        int tacno = 0;
+        while(tacno != 1){
+            System.out.println("Enter password");
+            String user_pass = reader.nextLine();
+                    
+            if(!correct_pass.equals(user_pass)){
+                System.out.println("Enter password");
+                 user_pass = reader.nextLine();
+            }else {
+                tacno = 1;
+            }
+        }
+        System.out.println("Secret");*/
+                
+        
+        
+       /* Scanner reader=new Scanner (System.in);
+        System.out.println("napisi prvi broj");
+        int num1=reader.nextInt();
+        System.out.println("napisi drugi broj");
+        int num2=reader.nextInt();
+        System.out.println("napisi treci broj");
+        int num3=reader.nextInt();
+        int sum=0;
+        sum=num1+num2+num3;
+        System.out.println(sum);*/
+       
+       
+       /* Scanner reader=new Scanner (System.in);
+        int sum=0;
+        int num;
+        while(true){
+             System.out.println("napisi broj");
+             num=Integer.parseInt(reader.nextLine());
+             if (num==0){
+                 break;
+             }
+             sum += num;
+              System.out.println("suma je " +sum);
+          
+        }
+          System.out.println("suma na kraju je "+sum); */
+       
+      /* Scanner reader= new Scanner(System.in);    
+       System.out.println("unesi prvi broj");
+        int first = Integer.parseInt(reader.nextLine());
+       System.out.println("unesi zadnji broj");
+       int second = Integer.parseInt(reader.nextLine());
+       while(first<=second){
+           System.out.println(first);
+           first++;
+           
+       } */
+      
+      /*Scanner reader=new Scanner(System.in);
+       System.out.println("unesi broj");
+       int n = Integer.parseInt(reader.nextLine());
+       int sum=1;
+       for (int i=1; i<=n; i++ ){
+           sum= sum + (int)Math.pow(2,i);
+       }
+        System.out.println("sum" + sum); */
+      
+     /* printText();       
+
+}
+    public static void printText(){
+        String novi="In the beginning there were the swamp, the hoe and Java.";
+        System.out.println(novi);
+    } */
+     
+   /* printStars(5);
+}
+    public static void printStars(int num){
+        for (int i=0;i<num;i++){
+            System.out.print("*");
+        }
+    }*/
+   
+   /*Scanner reader=new Scanner (System.in);
+   int rand_num=drawNumber();
+   System.out.println(rand_num);
+   int pokusaj=0;
+   System.out.println("pogadaj broj");
+   int guessnum=reader.nextInt();
+   while(guessnum!=rand_num){
+       if(guessnum>rand_num){
+           pokusaj+=1;
+           System.out.println("you need lower number"+pokusaj+"guess");
+       }else if (guessnum<rand_num){
+           pokusaj+=1;
+           System.out.println("you need higher number"+ pokusaj+ "guess");   
+       }
+       guessnum=reader.nextInt();
+       }
+   
+   System.out.println("congratulations, your guess is correct");
+    }
+    public static int drawNumber(){
+        Random rand= new Random();
+        int rand_num=rand.nextInt(100);
+        return rand_num;
+    }*/
+   
+    }
+    
+}
