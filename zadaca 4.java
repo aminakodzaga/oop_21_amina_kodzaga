dec4d1f366567edcf3a26f8e4fe94e522e84ca72
aminakodzaga <92090852+aminakodzaga@users.noreply.github.com>
2021-11-02 23:16:25 +0100

    Add files via upload

---
+++ zadaca 4.java
@@ -0,0 +1,166 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week.pkg4;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+*
+ *
+ * @author PC
+ *
+/*public class Dice{
+    private int numberOfSides;
+    private Random random;
+    
+    public Dice (int numberofSides){
+        random=new Random();
+        this.numberOfSides=numberOfSides;
+    }
+    public Dice (){
+        System.out.println("hello");
+        
+    }
+    public int roll{
+    return random.nextInt(this.numberOfSides)+1;
+}
+       public String toString(){
+       return "number of sides =" this.numberOfSides;
+       }
+}*/
+   
+     * @param args the command line arguments
+     
+    public static void main(String[] args) {
+        // TODO code application logic here
+     /*ArrayList<Integer>list1=new ArrayList<Integer>();
+      ArrayList<Integer>list2=new ArrayList<Integer>();
+      list1.add(4);
+      list1.add(5);
+      list1.add(8);
+      list2.add(7);
+      list2.add(9);
+      combine(list1,list2);
+      System.out.println(list1);
+      System.out.println(list2);
+      
+    }
+    public static void combine(ArrayList<Integer>list1,ArrayList<Integer>list2){
+        list1.addAll(list2);
+    }*/
+     
+      /*Dice d=new Dice(6);
+       int i =0;
+       while(i<10){
+       System.out.println(d.roll());
+       i++;*/
+
+/*LyyraCard card = new LyyraCard(10);
+        System.out.println(card);*/
+        
+        
+        /*LyyraCard card = new LyyraCard(50);
+        System.out.println(card);
+        card.payEconomical();
+        System.out.println(card);
+
+        card.payGourmet();
+        card.payEconomical();
+        
+        System.out.println(card);*/
+
+        
+      /*  card.payGourmet();
+        System.out.println(card);
+      
+        card.payGourmet();
+        System.out.println(card);*/
+        
+        /*card.loadMoney(15);
+        System.out.println(card);
+        
+        card.loadMoney(10);
+        System.out.println(card);
+        
+        card.loadMoney(200);
+        System.out.println(card);*/
+        
+        /*System.out.println("Pekka: "+ card);
+        card.loadMoney(-15);
+        System.out.println("Pekka: "+ card);*/
+
+        /*LyyraCard pekka = new LyyraCard(20, "Pekka");
+        LyyraCard brian = new LyyraCard(30, "Brian");
+     
+         pekka.payGourmet();
+         brian.payEconomical();
+         System.out.println(pekka);
+         System.out.println(brian);
+         
+         pekka.loadMoney(20);
+         brian.payGourmet();
+         
+         System.out.println(pekka);
+         System.out.println(brian);
+         
+         pekka.payEconomical();
+         pekka.payEconomical();
+         
+         brian.loadMoney(50);
+         System.out.println(pekka);
+         System.out.println(brian);
+}*/
+       
+    
+    
+    
+/*public class LyyraCard {
+    private double balance;
+    private String name; 
+    
+    public LyyraCard (double b){
+        this.balance = b;
+    }
+    
+    public LyyraCard (double b, String n){
+        this.balance = b;
+        this.name = n;
+    }
+    
+    public String toString(){
+        return  this.name + ": The card has " + this.balance + " euros";
+    }
+    
+    public void payEconomical(){
+        if(this.balance >= 2.5){
+            this.balance = this.balance - 2.5;
+        }
+    }
+    public void payGourmet(){
+        if(this.balance >= 4){
+             this.balance = this.balance - 4;
+        }
+    }
+    
+    public void loadMoney(double amount){
+        if(amount > 0){
+            this.balance = this.balance + amount;
+            if(this.balance > 150){
+                this.balance = 150;
+            }
+        }
+    }
+    
+}*/
+ 
+
+            
+
+    
+
+    
+
+    
+
